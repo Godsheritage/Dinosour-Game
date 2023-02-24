8a6ab975bb7d1d8fbe3c2acc3bf56c51b8788614
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2023-02-23 22:33:53 -0500

    updates to the Dice App

--- DGStarter/DetailViewController.swift
+++ DGStarter/DetailViewController.swift
@@ -9,8 +9,56 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    
+    var dinosaur : Dinosaur?
+    
+    
+    
+    @IBOutlet weak var dinosaurImageView: UIImageView!
+    
+    @IBOutlet weak var dinosaurNameLabel: UILabel!
+    
+    
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    
+    @IBOutlet weak var typeLabel: UILabel!
+
+    
+    @IBOutlet weak var factLabel1: UILabel!
+    @IBOutlet weak var factLabel2: UILabel!
+    @IBOutlet weak var factLabel3: UILabel!
+    @IBOutlet weak var factLabel4: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let dinosaur = dinosaur {
+            print(dinosaur.name)
+        }
+        if let dinosaur = dinosaur {
+            // Configure the dinosaur image and dynamic labels
+            dinosaurImageView.image = dinosaur.image
+            dinosaurNameLabel.text = dinosaur.name
+            typeLabel.text = dinosaur.type
+            weightLabel.text = String(dinosaur.weight) + " KG"
+            heightLabel.text = String(dinosaur.height) + " M"
+            factLabel1.text = String(dinosaur.diet)
+            factLabel2.text = String(dinosaur.era)
+            factLabel3.text = String(dinosaur.region)
+            factLabel4.text = String(dinosaur.speed) + " mph"
+        }
 
         // Do any additional setup after loading the view.
     }
